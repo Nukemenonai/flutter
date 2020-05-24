1f63089e4aa5805eb2a41d10adcc6964c0275410
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-05-23 21:34:37 -0500

    checking classes

---
+++ 4-classes.dart
@@ -0,0 +1,26 @@
+void main() {
+
+  final wolverine = Hero('Logan', 'Regeneration'); //instances declared with reserved word 'new' are optional
+  //final is to say Dart this variable will never change its value 
+  //properties can be changed but we cannot reassign wolverine to another value 
+
+  print(wolverine); 
+
+
+}
+
+//class names are first letter Capitalize or CamelCase
+class Hero {
+  String name;
+  String power; 
+
+  Hero(String nombre, String poder); {
+    // this is the constructor
+    this.name = nombre; 
+    this.power = poder; 
+    }
+  String toString() {
+    //method
+    return '${ this.name } - ${ this.power }';
+  }
+} 
